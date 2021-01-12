823322c14cdf1b88115edf85b8f51184571e22d9
Juan La Jara <wxomb@Juans-MacBook.local>
2021-01-11 18:03:39 -0600

    Add Poppins google font

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;900&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap"
+            rel="stylesheet"
+          />
           <GlobalStyles />
           {this.props.styleTags}
         </Head>
