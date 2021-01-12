745184f4fa1f1649d33cc25524a4d3a3a9fdf1b6
Juan La Jara <wxomb@Juans-MacBook.local>
2021-01-11 20:27:22 -0600

    Add Stepper component
    It all conveys into one simple API, example:
    `<Stepper steps={2} of={4} />`

---
+++ components/Stepper/index.js
@@ -0,0 +1,44 @@
+import {
+  StepBall,
+  StepLine,
+  Step as StyledStep,
+  Stepper as StyledStepper,
+} from './styles.js';
+
+const Step = (props) => {
+  return (
+    <StyledStep>
+      <StepBall type={props.type} />
+      {props.isLast ? null : <StepLine type={props.type} />}
+    </StyledStep>
+  );
+};
+
+const Stepper = (props) => {
+  if (props.steps === null || props.of === null) {
+    throw new Error('Stepper needs valid values');
+  }
+  if (!(props.of >= props.steps)) {
+    throw new Error('Of value should be greater or equal than step values');
+  }
+
+  const untaken = props.of - props.steps;
+  return (
+    <StyledStepper>
+      {Array.from({ length: props.steps }).map((_, index) => {
+        if (index === props.steps - 1 && untaken === 0) {
+          return <Step isLast type="taken" />;
+        }
+        return <Step type="taken" />;
+      })}
+      {Array.from({ length: untaken }).map((_, index) => {
+        if (index === untaken - 1) {
+          return <Step isLast type="untaken" />;
+        }
+        return <Step type="untaken" />;
+      })}
+    </StyledStepper>
+  );
+};
+
+export default Stepper;
---
+++ components/Stepper/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const backgroundColorPriority = {
+  taken: '#4EB5A2',
+  untaken: '#C7CAD1',
+};
+
+export const StepBall = styled('div')`
+  background-color: ${(props) =>
+    backgroundColorPriority[props.type || 'untaken']};
+  border-radius: 0.625rem;
+  height: 1.25rem;
+  width: 1.25rem;
+`;
+
+export const StepLine = styled('div')`
+  background-color: ${(props) =>
+    backgroundColorPriority[props.type || 'untaken']};
+  height: 0.0625rem;
+  width: 4.0625rem;
+`;
+
+export const Step = styled('div')`
+  align-items: center;
+  display: flex;
+`;
+
+export const Stepper = styled('div')`
+  align-items: center;
+  display: flex;
+`;
