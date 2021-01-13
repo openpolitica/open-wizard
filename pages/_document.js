0637263b1381bcb3d7f67bc1dc730acc976f1b5f
Juan La Jara <wxomb@Juans-MacBook.local>
2021-01-13 00:02:04 -0600

    Add normalize
    Hopefully nothing changes 😅.

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
+          <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+            rel="stylesheet"
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;900&display=swap"
