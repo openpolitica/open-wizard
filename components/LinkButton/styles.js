54c47769ebdfc323f0e4ca46fb86731cbe6f01a4
Juan La Jara <wxomb@Juans-MacBook.local>
2021-01-13 00:20:39 -0600

    Add LinkButton component
    We add 10px padding as we need buttons to have fairly big hit boxes on mobile.

---
+++ components/LinkButton/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Button = styled('button')`
+  background-color: transparent;
+  border: none;
+  color: #4EB5A2;
+  padding: .625rem;
+  text-decoration: underline;
+`;
