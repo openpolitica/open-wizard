54224b195fc2ab98a3dfcf065bb07b9dcdbb02f7
Juan La Jara <wxomb@Juans-MacBook.local>
2021-01-14 14:20:20 -0600

    Add first version of navigation
    We might use a localstorage approach as I think it's the simplest way to tackle
    state for this use case.

--- pages/steps/1/index.js
+++ pages/steps/1/index.js
@@ -1,4 +1,9 @@
+import Router from 'next/router';
 import * as Styled from './styles';
+
+const onContinueClick = event => {
+  Router.push('/steps/2');
+};
 
 export default function Step1() {
   return (
@@ -18,7 +23,7 @@ export default function Step1() {
         </Styled.Select>
         <Styled.LinkButton>Vivo en el extranjero</Styled.LinkButton>
       </Styled.Step>
-      <Styled.Button>Continuar</Styled.Button>
+      <Styled.Button onClick={onContinueClick}>Continuar</Styled.Button>
     </Styled.Container>
   );
 }
---
+++ pages/steps/2/index.js
@@ -0,0 +1,5 @@
+import * as Styled from './styles';
+
+export default function Step2() {
+  return <div></div>;
+}
