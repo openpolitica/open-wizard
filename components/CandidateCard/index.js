b5d4655b1ebc5f0c1dc02075c88d1457e91689cf
Juan La Jara <juanlajara@gmail.com>
2021-01-14 22:37:14 -0600

    Pass down props and use more realistic naming

--- components/CandidateCard/index.js
+++ components/CandidateCard/index.js
@@ -1,14 +1,16 @@
 import { Card, PartyIcon, NumberIcon, ProfileIcon, Fullname } from './styles';
 
+const profileIconBaseUrl = 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/';
+
 const CandidateCard = (props) => {
   return (
-    <Card>
+    <Card {...props}>
       <PartyIcon src={props.candidateParty || 'https://picsum.photos/32'} />
       <NumberIcon>{props.candidateNumber || '5'}</NumberIcon>
       <ProfileIcon
         src={
-          props.profileImage ||
-          'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/134127.jpg'
+          `${profileIconBaseUrl}${props.profileImageId}.jpg` ||
+          `${profileIconBaseUrl}134127.jpg`
         }
       />
       <Fullname>{props.candidateFullname || 'Nombre de candidato'}</Fullname>
