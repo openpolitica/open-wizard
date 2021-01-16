02749e4eb2e9576c207539b34a246da1c837a199
Juan La Jara <juanlajara@gmail.com>
2021-01-15 22:51:26 -0600

    Add Loading component
    The actual svg is not optimal so we'll update it in the future.

---
+++ components/Loading/index.js
@@ -0,0 +1,11 @@
+import * as Styled from './styles';
+
+const Loading = (props) => {
+  return (
+    <Styled.Loading>
+      <Styled.Loader src="../images/icons/loader.svg" />
+    </Styled.Loading>
+  );
+};
+
+export default Loading;
