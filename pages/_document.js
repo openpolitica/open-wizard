de04618be2491b08283d26b181bca2da0d72c9b9
almeald <almenaldana@gmail.com>
2021-01-17 01:46:35 -0500

    added poppins 700 font

--- pages/_document.js
+++ pages/_document.js
@@ -27,7 +27,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
           <GlobalStyles />
