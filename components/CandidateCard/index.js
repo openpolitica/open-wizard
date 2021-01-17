80ac79be7a706a3e9f5e36806f83a5da0730227e
Juan La Jara <juanlajara@gmail.com>
2021-01-17 12:03:57 -0600

    Update `partyIconSource`
    Now it defaults in the same function and is being exported so it can be reused.

--- components/CandidateCard/index.js
+++ components/CandidateCard/index.js
@@ -2,20 +2,18 @@ import { Card, PartyIcon, NumberIcon, ProfileIcon, Fullname } from './styles';
 
 const profileIconBaseUrl = 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/';
 
-const partyIconSource = (candidateParty) =>
-  `/images/party-logos/${candidateParty
-    .split(' ')
-    .join('-')
-    .toLowerCase()}.jpg`;
+export const partyIconSource = (candidateParty) =>
+  candidateParty
+    ? `/images/party-logos/${candidateParty
+        .split(' ')
+        .join('-')
+        .toLowerCase()}.jpg`
+    : 'https://picsum.photos/32';
 
 const CandidateCard = (props) => {
   return (
     <Card {...props}>
-      <PartyIcon
-        src={
-          partyIconSource(props.candidateParty) || 'https://picsum.photos/32'
-        }
-      />
+      <PartyIcon src={partyIconSource(props.candidateParty)} />
       <NumberIcon>{props.candidateNumber || '5'}</NumberIcon>
       <ProfileIcon
         src={
