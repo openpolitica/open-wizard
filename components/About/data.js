3eb77a13b75a24c11a1475b9bd666b5521bfa2ba
almeald <almenaldana@gmail.com>
2021-01-21 02:54:37 -0500

    added about page and components with team data

---
+++ components/About/data.js
@@ -0,0 +1,88 @@
+export const team = [
+  {
+    photo: '/images/photos/random.png',
+    role: 'Orchestrator',
+    name: 'Alessandra Rojas',
+  },
+  {
+    photo: '/images/photos/Rodrigo-Torres.png',
+    name: 'Rodrigo Torres',
+    role: 'Twitter Master',
+  },
+  {
+    photo: '/images/photos/Fernando-Ruiz.jpeg',
+    name: 'Fernando Ruiz',
+    role: 'Contenido',
+  },
+  {
+    photo: '/images/photos/Lucia-Valdivieso.jpg',
+    name: 'Lucía Valdivieso',
+    role: 'Contenido',
+  },
+  {
+    photo: '/images/photos/random.png',
+    name: 'Jorge Quilcate',
+    role: 'Data',
+  },
+  {
+    photo: '/images/photos/Juan-LaJara.png',
+    name: 'Juan La Jara',
+    role: 'Frontend Dev',
+  },
+  {
+    photo: '/images/photos/Gianpietro-Lavado.jpg',
+    name: 'Gianpietro Lavado',
+    role: 'Backend Dev',
+  },
+  {
+    photo: '/images/photos/Diego-Marquina.jpg',
+    name: 'Diego Marquina',
+    role: 'Backend Dev',
+  },
+  {
+    photo: '/images/photos/random.png',
+    name: 'Arturo Via y Rada',
+    role: 'Product Designer',
+  },
+  {
+    photo: '/images/photos/random.png',
+    name: 'Retaco The Cat',
+    role: 'CEO',
+  },
+];
+
+export const collaborators = [
+  {
+    photo: '/images/photos/Almendra-Maravi.JPG',
+    name: 'Almendra Maraví',
+    role: 'Developer',
+  },
+  {
+    photo: '/images/photos/random.png',
+    name: 'Carmen Fariñez',
+    role: 'Developer',
+  },
+];
+
+export const mentors = [
+  {
+    photo: '/images/photos/Elizabeth-Carhuatocto.jpeg',
+    name: 'Elizabeth Carhuatocto',
+    role: 'UX Designer',
+  },
+  {
+    photo: '/images/photos/random.png',
+    name: 'Kristian Lopez',
+    role: 'Role',
+  },
+  {
+    photo: '/images/photos/Lorena-Vallejos.jpeg',
+    name: 'Lorena Vallejos',
+    role: 'Product Manager',
+  },
+  {
+    photo: '/images/photos/Luis-Enrique.jpg',
+    name: 'Luis Enrique Flórez',
+    role: 'Role',
+  },
+];
---
+++ components/TeamCard/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import Image from 'next/image';
+
+export const Photo = styled(Image)`
+  border-radius: 2.5rem;
+  margin-bottom: 0.5rem;
+  object-fit: cover;
+`;
+
+export const Name = styled.p`
+  color: #475065;
+  font-size: 0.875rem;
+  font-weight: 600;
+  margin-top: 0.5rem;
+`;
+
+export const Role = styled.p`
+  color: #475065;
+  font-size: 0.875rem;
+  font-weight: normal;
+  margin-top: 0.25rem;
+`;
