08005ff0ff4046c43daee177a417509e2fee4eb8
Juan La Jara <juanlajara@gmail.com>
2021-01-24 23:21:49 -0600

    Add FilterModal component
    fix Genre selector as it shouldn't be a radio button.

---
+++ components/FilterModal/logicMaps.js
@@ -0,0 +1,46 @@
+export const toInitialEducationState = () => ({
+  primary: false,
+  secondary: false,
+  high: false,
+  post: false,
+});
+
+export const toPrimary = () => ({
+  primary: true,
+  secondary: false,
+  high: false,
+  post: false,
+});
+
+export const upToPrimaryFilterIn = () => ['upToPrimary'];
+export const upToPrimaryFilterOut = () => [];
+
+export const toSecondary = () => ({
+  primary: true,
+  secondary: true,
+  high: false,
+  post: false,
+});
+
+export const upToSecondaryFilterIn = () => ['upToSecondary'];
+export const upToSecondaryFilterOut = () => ['upToPrimary'];
+
+export const toHighSchool = () => ({
+  primary: true,
+  secondary: true,
+  high: true,
+  post: false,
+});
+
+export const upToHighSchoolFilterIn = () => ['upToHighSchool'];
+export const upToHighSchoolFilterOut = () => ['upToSecondary'];
+
+export const toPostgraduate = () => ({
+  primary: true,
+  secondary: true,
+  high: true,
+  post: true,
+});
+
+export const upToPostgraduateFilterIn = () => ['upToPostgraduate'];
+export const upToPostgraduateFilterOut = () => ['upToHighSchool'];
