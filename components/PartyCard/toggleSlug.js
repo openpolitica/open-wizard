7f861988425cdbb098acdded3e0349f2920d3650
Juan La Jara <juanlajara@gmail.com>
2021-02-06 21:47:36 -0600

    Update with party names that need special treatment
    Given that they use dashes in their names 🤦‍♂️.

--- components/PartyCard/toggleSlug.js
+++ components/PartyCard/toggleSlug.js
@@ -3,6 +3,12 @@ const toggleSlug = (text) => {
   if (text === 'frente-popular-agricola-fia-del-peru---frepap') {
     return 'FRENTE POPULAR AGRICOLA FIA DEL PERU - FREPAP';
   }
+  if (text === 'partido-popular-cristiano---ppc') {
+    return 'PARTIDO POPULAR CRISTIANO - PPC';
+  }
+  if (text === 'avanza-pais---partido-de-integracion-social') {
+    return 'AVANZA PAIS - PARTIDO DE INTEGRACION SOCIAL';
+  }
   if (text.includes(' ')) {
     return text.split(' ').join('-').toLowerCase();
   }
