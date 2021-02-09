82926914e30967bfb8e99a70db600ae9f1bfb81e
gianpietro1 <gianpietro1@gmail.com>
2021-02-08 23:03:48 -0500

    uses next/dynamic to remove SSR from all pages

--- pages/steps/3/index.js
+++ pages/steps/3/index.js
@@ -1,1 +1,8 @@
-export { default } from 'components/Steps/3';
+import dynamic from 'next/dynamic';
+
+const Step3 = dynamic(() => import('components/Steps/3'), {
+  ssr: false,
+});
+
+const nossrStep3 = () => <Step3 />;
+export default nossrStep3;
