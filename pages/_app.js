b805298615caba9b5e9322cc626c5c4395185a09
Juan La Jara <juanlajara@gmail.com>
2021-02-10 00:19:13 -0600

    Add Hotjar support
    We just add the script, let's see if that's good enough or we should configure
    some other things.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import Head from 'next/head';
+import { useHotjar } from 'react-use-hotjar';
 
 function Votu({ Component, pageProps }) {
   return (
@@ -12,4 +13,17 @@ function Votu({ Component, pageProps }) {
   );
 }
 
-export default Votu;
+const logger = (value) =>
+  console.info(`%c${value}`, 'background: #222; color: #bada55');
+
+const AppWithHotjar = (props) => {
+  const { initHotjar } = useHotjar();
+
+  useEffect(() => {
+    initHotjar(process.env.hotjar.id, process.env.hotjar.version, logger);
+  });
+
+  return <Votu {...props} />;
+};
+
+export default AppWithHotjar;
