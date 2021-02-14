2aefbafe7febef7f892482c2731abe9aba4d9a01
Juan La Jara <juanlajara@gmail.com>
2021-02-14 00:02:33 -0600

    Use NextJS html element offsetHeight as BaseModal overlay height
    We also update the name of the container as it has more meaning.

--- components/BaseModal/styles.js
+++ components/BaseModal/styles.js
@@ -9,23 +9,22 @@ export const Modal = styled('section').attrs((props) => ({
   border: 1px solid #f1f2f3;
   box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.12);
   border-radius: 4px;
-  height: 90vh;
   left: 50%;
-  overflow: scroll;
   position: absolute;
   transition: all linear 0.3s;
   transform: translateX(-50%);
   width: 20.5rem;
 `;
 
-export const ModalContainer = styled('div').attrs((props) => ({
+export const Overlay = styled('div').attrs((props) => ({
   style: {
-    height: props.isOpen ? '100vh' : '0',
     opacity: props.isOpen ? '1' : '0',
     zIndex: props.isOpen ? '10' : '-1',
   },
 }))`
   background: rgba(255, 255, 255, 0.9);
+  min-height: ${(props) =>
+    props.nextHeight ? `${props.nextHeight}px` : '150vh'};
   position: absolute;
   width: 100vw;
 `;
