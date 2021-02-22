c863e8218ece73b3f53fdb1991f8d4016a700216
Juan La Jara <juanlajara@gmail.com>
2021-02-21 18:06:58 -0600

    Add Matomo tracking script
    We also refactor script initializations with a more functional approach.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Fragment, useEffect } from 'react';
 import Head from 'next/head';
 import { useHotjar } from 'react-use-hotjar';
+import { init as initMatomo } from '@socialgouv/matomo-next';
 
 function Votu({ Component, pageProps }) {
   return (
@@ -16,16 +17,27 @@ function Votu({ Component, pageProps }) {
 const logger = (value) =>
   console.info(`%c${value}`, 'background: #222; color: #bada55');
 
-const VotuWithHotjar = (props) => {
+const withHotjar = (Component) => (props) => {
   const { initHotjar } = useHotjar();
 
   useEffect(() => {
     initHotjar(process.env.hotjar.id, process.env.hotjar.version, logger);
   });
 
-  return <Votu {...props} />;
+  return <Component {...props} />;
+};
+
+const withMatomo = (Component) => (props) => {
+  useEffect(() => {
+    initMatomo({
+      url: process.env.matomo.url,
+      siteId: process.env.matomo.siteId,
+    });
+  });
+
+  return <Component {...props} />;
 };
 
 export default process.env.NEXT_PUBLIC_ENVIRONMENT === 'local'
   ? Votu
-  : VotuWithHotjar;
+  : withMatomo(withHotjar(Votu));
