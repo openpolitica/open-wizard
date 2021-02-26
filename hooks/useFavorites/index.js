2e3b75100acb7e3c1b805fcbc0ee1099bca54dc7
Juan La Jara <juanlajara@gmail.com>
2021-02-26 00:41:03 -0600

    Add useFavorites hook

---
+++ hooks/useFavorites/index.js
@@ -0,0 +1,49 @@
+import { createContext } from 'react';
+import { usePersistedReducer } from 'hooks/usePersistedReducer';
+
+export const FavoritesContext = createContext();
+
+const initialFavoritesState = {
+  data: [],
+};
+
+const reducer = (state, action) => {
+  if (action.type === 'addFavorite') {
+    return { data: [...state.data, action.value] };
+  }
+  if (action.type === 'removeFavoriteBySystemId') {
+    const favoriteIndex = state.favorites.findIndex(
+      (favorite) => favorite.systemId === action.value,
+    );
+    return {
+      data: [
+        ...state.data.slice(0, favoriteIndex),
+        ...state.data.slice(favoriteIndex + 1),
+      ],
+    };
+  }
+  if (action.type === 'resetFavorites') {
+    return initialFavoritesState;
+  }
+};
+
+export const FavoritesProvider = ({ children }) => {
+  const [state, dispatch] = usePersistedReducer(
+    reducer,
+    'op.favorites',
+    initialFavoritesState,
+  );
+
+  return (
+    <FavoritesContext.Provider
+      value={{
+        favorites: state.data,
+        addFavorite: (value) => dispatch({ type: 'addFavorite', value }),
+        removeFavoriteBySystemId: (value) =>
+          dispatch({ type: 'removeFavoriteBySystemId', value }),
+        resetFavorites: (value) => dispatch({ type: 'resetFavorites' }),
+      }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
