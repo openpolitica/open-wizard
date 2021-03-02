59a7675d15b15910e5fba87a07f693038b13e8c2
gianpietro1 <gianpietro1@gmail.com>
2021-03-02 11:21:27 -0500

    self-hosted matomo in new domain

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = {
       version: 6,
     },
     matomo: {
-      url: 'http://analytics.openpolitica.com/',
+      url: 'https://matomo.openpolitica.com/',
       siteId: 1,
     },
   },
