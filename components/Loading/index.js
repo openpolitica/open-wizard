9964aab407e5e7c88ae26b4cf70a499e5416606a
almeald <almenaldana@gmail.com>
2021-03-03 03:19:34 -0500

    fix loader

--- components/Loading/index.js
+++ components/Loading/index.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import * as Styled from './styles';
 
 const Loading = (props) => {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((oldProgress) => {
+        const newProgress = oldProgress + 40;
+
+        if (newProgress === 100) {
+          clearInterval(interval);
+        }
+        return newProgress;
+      });
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Styled.Loading>
       <Styled.Loader src="../images/icons/loader.svg" />
+      <Styled.Progress value={value} />
     </Styled.Loading>
   );
 };
--- components/Loading/styles.js
+++ components/Loading/styles.js
@@ -1,22 +1,33 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
-export const Loading = styled('section')`
+export const Loading = styled('div')`
+  progress[value] {
+    appearance: none;
+
+    ::-webkit-progress-bar {
+      background-color: #fff;
+      border: 1px solid #e3e5e8;
+      border-radius: 1.25rem;
+      height: 0.5rem;
+    }
+
+    ::-webkit-progress-value {
+      background-color: #04102f;
+      border-radius: 1.25rem;
+      height: 0.375rem;
+    }
+  }
+
+  align-items: center;
+  display: flex;
+  flex-direction: column;
   height: 100vh;
-  line-height: 50vh;
-  text-align: center;
-  width: 100vw;
-`;
-
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
+  justify-content: center;
 `;
 
 export const Loader = styled('img')`
-  animation: ${rotate} 2s linear infinite;
+  margin: 0 auto 1.5rem;
+  width: 11.125rem;
 `;
+
+export const Progress = (props) => <progress value={props.value} max="100" />;
