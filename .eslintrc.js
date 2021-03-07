33b558ba1855fb1cae13699b9157557700ba9e7e
Juan La Jara <juanlajara@gmail.com>
2021-03-07 01:12:51 -0600

    Add Eslint
    With simple rules like no-unused-vars so they help us cleaning up the code
    a little bit.

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'react-app',
+  rules: {
+    'no-console': 'warn',
+    'import/newline-after-import': ['warn', { count: 1 }],
+    'import/no-unused-modules': [
+      'warn',
+    ],
+    'react-hooks/rules-of-hooks': 'off',
+  },
+};
