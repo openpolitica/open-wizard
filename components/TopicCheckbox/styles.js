29ce9bfb20b9a43f59ee2f29be8ad0ed878d39f3
Juan La Jara <juanlajara@gmail.com>
2021-03-07 00:12:23 -0600

    Add TopicCheckbox component
    And related svg icons.
    We add and index file to `public/images/icons/topics` so it's easier to import them.`

---
+++ components/TopicCheckbox/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Label = styled('label')`
+  align-items: center;
+  background: white;
+  border-radius: 5px;
+  box-shadow: ${(props) =>
+    props.checked
+      ? '0px 4px 8px rgba(0, 0, 0, 0.12)'
+      : '0px 2px 2px rgba(0, 0, 0, 0.12)'};
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  font-family: 'Poppins', sans-serif;
+  height: 7rem;
+  justify-content: space-evenly;
+  width: 9rem;
+`;
+
+export const Checkbox = styled('input').attrs((props) => ({
+  type: 'checkbox',
+}))`
+  appearance: none;
+  display: none;
+`;
+
+export const TopicIcon = styled('svg')`
+  color: ${(props) => (props.checked ? '#5BC13E' : '#C7CAD1')};
+  path {
+    fill: currentColor;
+    stroke: currentColor;
+  }
+`;
---
+++ public/images/icons/topics/index.js
@@ -0,0 +1,6 @@
+export { default as EducationIcon } from './education.svg';
+export { default as HealthIcon } from './health.svg';
+export { default as EconomyIcon } from './economy.svg';
+export { default as GobernabilityIcon } from './gobernability.svg';
+export { default as SecurityIcon } from './security.svg';
+export { default as EnvironmentIcon } from './environment.svg';
