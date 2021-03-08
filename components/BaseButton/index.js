51aa7a9810135bca2fb9182649530a4fcf43c7a2
almeald <almenaldana@gmail.com>
2021-03-08 02:24:55 -0500

    Revert "add icon prop"
    This reverts commit d6e448beff3265c621fdc65b95ddce318bc529eb.

--- components/BaseButton/index.js
+++ components/BaseButton/index.js
@@ -3,7 +3,6 @@ import { StyledButton } from './styles.js';
 const BaseButton = (props) => {
   return (
     <StyledButton {...props}>
-      {props.icon || null}
       {typeof props.children === 'string' ? props.children : props.text}
     </StyledButton>
   );
