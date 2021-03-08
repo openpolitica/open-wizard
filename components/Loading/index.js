50ea587001c3ac771bc6109298ba6197577700fb
Almendra Maraví <60791759+AlmeAld@users.noreply.github.com>
2021-03-08 00:27:23 -0500

    Add new loader (#214)

--- components/Loading/index.js
+++ components/Loading/index.js
@@ -3,7 +3,9 @@ import * as Styled from './styles';
 const Loading = (props) => {
   return (
     <Styled.Loading>
-      <Styled.Loader src="../images/icons/loader.svg" />
+      <Styled.Loader src="/images/icons/votu_logo.svg" width="90" height="35" />
+      <Styled.FadingEffect />
+      <p>Cargando...</p>
     </Styled.Loading>
   );
 };
--- components/Loading/styles.js
+++ components/Loading/styles.js
@@ -1,22 +1,33 @@
 import styled, { keyframes } from 'styled-components';
+import Image from 'next/image';
 
 export const Loading = styled('section')`
-  height: 100vh;
-  line-height: 50vh;
-  text-align: center;
-  width: 100vw;
+  line-height: 2rem;
+  margin: 11.25rem auto;
+  overflow: hidden;
+  position: relative;
+  width: 5.625rem;
 `;
 
 const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
+  0% {width:100%}
+  40% {width:0%}
+  60% {width:0%;}
+  100% {width:100%;}
 `;
 
-export const Loader = styled('img')`
-  animation: ${rotate} 2s linear infinite;
+export const FadingEffect = styled('div')`
+  animation: ${rotate} 5s ease alternate infinite;
+  background: #f1f2f3;
+  bottom: 50%;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 100%;
+  -moz-animation: ${rotate} 5s ease-in alternate infinite;
+  -webkit-animation: ${rotate} 5s ease-in alternate infinite;
+  -ms-animation: ${rotate} 5s ease-in alternate infinite;
+  -o-animation: ${rotate} 5s ease-in alternate infinite;
 `;
+
+export const Loader = styled(Image)``;
