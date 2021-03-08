5a8a97c1942c9f608b7ed62d6a6ade4d42251f6a
almeald <almenaldana@gmail.com>
2021-03-07 23:19:33 -0500

    fix loader

--- components/Loading/index.js
+++ components/Loading/index.js
@@ -3,7 +3,9 @@ import * as Styled from './styles';
 const Loading = (props) => {
   return (
     <Styled.Loading>
-      <Styled.Loader src="../images/icons/loader.svg" />
+      <Styled.Loader src={'/images/icons/votu_logo.svg'} />
+      <Styled.FadingEffect />
+      <p>Cargando...</p>
     </Styled.Loading>
   );
 };
