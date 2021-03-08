d6e448beff3265c621fdc65b95ddce318bc529eb
almeald <almenaldana@gmail.com>
2021-03-07 22:48:58 -0500

    add icon prop

--- components/BaseButton/index.js
+++ components/BaseButton/index.js
@@ -3,6 +3,7 @@ import { StyledButton } from './styles.js';
 const BaseButton = (props) => {
   return (
     <StyledButton {...props}>
+      {props.icon || null}
       {typeof props.children === 'string' ? props.children : props.text}
     </StyledButton>
   );
