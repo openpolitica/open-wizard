590ed69f120fc05cbbcd9a66d68859b9cdc9d6fb
almeald <almenaldana@gmail.com>
2021-03-13 02:08:37 -0500

    change about content

--- components/About/index.js
+++ components/About/index.js
@@ -1,53 +1,122 @@
-import TeamListUl from 'components/TeamCard';
-import Header from 'components/Header';
-import { team, mentors } from './data';
 import Footer from 'components/Footer';
 import * as Styled from './styles';
 
 export default function About() {
   return (
-    <>
-      <Header />
-      <Styled.Container>
-        <Styled.Main>
-          <Styled.Title>
-            Votu fue creado por{' '}
-            <Styled.GreenSpan>Open Política.</Styled.GreenSpan>
-          </Styled.Title>
-          <Styled.Subtitle>
-            Transparentamos la información política en el Perú.
-          </Styled.Subtitle>
-        </Styled.Main>
-        <Styled.TeamSection>
-          <Styled.TitleSection>El equipo</Styled.TitleSection>
-          <Styled.ListTeam>
-            {team.map((team) => (
-              <li key={team.name}>
-                <TeamListUl {...team} />
-              </li>
-            ))}
-          </Styled.ListTeam>
-        </Styled.TeamSection>
-        <Styled.MentorsSection>
-          <Styled.TitleSection>Mentores</Styled.TitleSection>
-          <Styled.ListTeam>
-            {mentors.map((mentors) => (
-              <li key={mentors.name}>
-                <TeamListUl {...mentors} />
-              </li>
-            ))}
-          </Styled.ListTeam>
-        </Styled.MentorsSection>
-        <div style={{ marginTop: '56px' }}>
-          <Styled.PartOfText>Fuimos parte de:</Styled.PartOfText>
-          <Styled.Link200
-            href="https://www.linkedin.com/company/200org/"
+    <Styled.Container>
+      <Styled.Header />
+      <Styled.Content>
+        <Styled.Title>Algunos detalles sobre el proyecto</Styled.Title>
+        <Styled.Paragraph>
+          <strong>VOTU </strong>
+          es el primer producto digital de{' '}
+          <Styled.ExternalLink
+            href="http://www.openpolitica.com"
             target="_blank">
-            <Styled.Image200 src="/images/200.png" />
-          </Styled.Link200>
-        </div>
-      </Styled.Container>
+            Open Política.
+          </Styled.ExternalLink>
+        </Styled.Paragraph>
+        <Styled.Paragraph>
+          Open Política es un colectivo que busca empoderar a las peruanas y
+          peruanos transparentando la información política a través de
+          herramientas digitales. Nuestro equipo está conformado por personas
+          dedicadas en proveer a las y los peruanos información transparente,
+          confiable y de calidad.
+        </Styled.Paragraph>
+        <Styled.Paragraph>
+          Los datos de hojas de vida de las y los candidatos, presentados en la
+          plataforma web de votu.pe, provienen de fuentes públicas,
+          específicamente del{' '}
+          <strong>
+            Jurado Nacional de Elecciones{' '}
+            <Styled.ExternalLink
+              href="https://declara.jne.gob.pe"
+              target="_blank">
+              (JNE)
+            </Styled.ExternalLink>
+            , el Poder Judicial y el Diario El Comercio{' '}
+            <Styled.ExternalLink
+              href="https://especiales.elcomercio.pe/?q=especiales/elecciones-2021-lista-candidatos-presidencia-congreso-parlamento-andino-tu-decides-nndd-ecvisual-ecpm/comparativo-lima.html"
+              target="_blank">
+              (El Comercio)
+            </Styled.ExternalLink>
+          </strong>
+          . Asimismo, para determinar el tipo de experiencia laboral, realizamos
+          un análisis de identificadores de palabras.
+        </Styled.Paragraph>
+        <Styled.Paragraph>
+          En el caso del cuestionario de propuestas, se incluyó en el análisis a
+          los 10 candidatos presidenciales con mayor intención de voto
+          presidencial según las últimas 3 encuestas publicadas al 10 de marzo
+          de 2021, específicamente:{' '}
+          <strong>
+            <Styled.ExternalLink
+              href="https://www.cpi.pe/images/upload/paginaweb/archivo/23/op_nac_inf_febrero2021.pdf"
+              target="_blank">
+              CPI (Campo 17-19 de febrero 2021)
+            </Styled.ExternalLink>{' '}
+            y{' '}
+            <Styled.ExternalLink
+              href="https://iep.org.pe/noticias/encuesta-de-opinion-febrero-2021/"
+              target="_blank">
+              IEP (Campo 19 - 23 de febrero 2021)
+            </Styled.ExternalLink>{' '}
+            y{' '}
+            <Styled.ExternalLink
+              href="https://peru21.pe/politica/encuesta-datum-el-21-de-peruanos-no-sabe-por-quien-votar-para-presidente-elecciones-2021-datum-urpi-torrado-noticia/"
+              target="_blank">
+              DATUM (Campo 5-7 de marzo de 2021).{' '}
+            </Styled.ExternalLink>
+          </strong>
+          En el caso haya un cambio en la lista de los 10 hasta marzo 2021, se
+          agregará al candidato adicional. La fuente principal de información
+          fueron los planes de gobierno presentados por los partidos políticos
+          ante el Jurado Nacional de Elecciones y disponibles{' '}
+          <Styled.ExternalLink
+            href="https://drive.google.com/drive/folders/1RiqHdqcj5McYOyFilHZiLQuyLUIvhI0w"
+            tar="_blank">
+            aquí
+          </Styled.ExternalLink>
+          . Adicionalmente, la información se completó con entrevistas en casos
+          específicos para establecer:
+          <Styled.InfoList type="i">
+            <li>
+              Postura del candidato presidencial sobre la vacancia presidencial
+              del 9 de noviembre de 2021;
+            </li>
+            <li>
+              Postura del candidato presidencial sobre la discriminalización de
+              causales de aborto y matrimonio civil o igualitario, siempre que
+              no esté explícita la postura del partido en el plan de gobierno;
+            </li>
+            <li>
+              Para revisar la postura del partido sobre un tema que requirió
+              mayor aclaración sobre el contenido en el plan de gobierno.
+            </li>
+          </Styled.InfoList>
+        </Styled.Paragraph>
+        <Styled.Paragraph>
+          En Open Política priorizamos la seguridad de datos de nuestros
+          usuarios. Por eso, en Votu no almacenamos ni queremos almacenar de
+          ninguna forma las opciones marcadas como favoritas de los usuarios ni
+          asociamos direcciones de IP a los mismos. Las selecciones de los
+          usuarios se guardan de manera directa en el navegador del usuario y
+          VOTU no tiene manera de enterarse de su selección.
+        </Styled.Paragraph>
+        <Styled.Paragraph>
+          Debido a la continua actualización de los datos que varía de acuerdo a
+          cada fuente, los cambios en las fuentes oficiales pueden tardar unos
+          días en ser actualizados en nuestra página web votu.pe. En caso
+          encuentres algún error en la plataforma web votu.pe, o tienes alguna
+          sugerencia, puedes comunicarte con nosotros a{' '}
+          <Styled.ExternalLink href="mailto:  openpoliticaperu@gmail.com?subject = Feedback&body = Message">
+            openpoliticaperu@gmail.com
+          </Styled.ExternalLink>
+          .
+        </Styled.Paragraph>
+        <Styled.Paragraph>El Equipo de Open Política </Styled.Paragraph>
+      </Styled.Content>
       <Footer />
-    </>
+    </Styled.Container>
   );
 }
--- components/About/styles.js
+++ components/About/styles.js
@@ -1,89 +1,41 @@
 import styled from 'styled-components';
-import Image from 'next/image';
+import BaseHeader from 'components/Header';
+import BaseTitle from 'components/BaseTitle';
 
-export const Container = styled('div')`
-  background: #fafafa;
-  font-family: 'Poppins', sans-serif;
-  padding: 1rem 1.5rem 3rem;
-  text-align: center;
-`;
-
-export const Main = styled('main')`
-  background-color: #04102f;
-  border-radius: 1rem;
-  color: #fff;
-  padding: 1.5rem;
-  text-align: initial;
-  margin-bottom: 0.875rem;
-`;
-
-export const Title = styled('h1')`
-  font-weight: 600;
-  font-size: 1.25rem;
-  line-height: 1.625rem;
-  margin: 0;
-  max-width: 12.5rem;
-`;
-
-export const GreenSpan = styled('span')`
-  color: #4eb5a2;
-`;
-
-export const Subtitle = styled('p')`
-  font-size: 0.875rem;
-  color: #c7cad1;
-  margin: 0;
-  margin-top: 0.5rem;
-`;
-
-const Section = styled('section')`
-  padding: 1.5rem 1.5rem 1.875rem;
-  margin-bottom: 1.5rem;
-  border-radius: 1rem;
-`;
-
-export const TeamSection = styled(Section)`
-  border: 1px solid #e3e5e8;
-  background-color: #f7f7f7;
-`;
-
-export const TitleSection = styled('h2')`
-  margin: 0px;
-  font-weight: 600;
-  font-size: 1.25rem;
-`;
-
-export const ListTeam = styled('ul')`
+export const Container = styled('section')`
+  align-items: center;
+  background-color: #fafafa;
   display: flex;
-  flex-wrap: wrap;
-  margin: 0px 0px 0 -0.9375rem;
-  padding: 0;
-  width: calc(100% + 0.9375rem);
-  & li {
-    flex: 1 1 44.5%;
-    list-style: none;
-    max-width: 44.5%;
-    margin: 1.5rem 0px 0 0.9375rem;
-  }
+  flex-direction: column;
 `;
 
-export const MentorsSection = styled(Section)`
-  border: 1px solid #f9d5d5;
-  background-color: #fdf4f4;
+export const Header = styled(BaseHeader)`
+  width: 100%;
 `;
 
-export const PartOfText = styled('p')`
-  color: #475065;
-  font-size: 0.875rem;
+export const Content = styled('div')`
+  padding: 2.5rem 1.5rem;
+`;
+
+export const Title = styled(BaseTitle)`
+  font-size: 1.5rem;
   font-weight: 600;
-  margin: 0;
-  margin-bottom: 0.5rem;
+  line-height: 2rem;
+  margin-bottom: 1rem;
+  margin-right: 5rem;
 `;
 
-export const Image200 = (props) => (
-  <Image height="116" width="116" alt="logo 200" {...props} />
-);
+export const Paragraph = styled('p')`
+  color: #475065;
+  font-family: 'Poppins', sans-serif;
+  font-size: 1rem;
+  line-height: 1.375rem;
+`;
 
-export const Link200 = styled('a')`
-  text-decoration: none;
-)`;
+export const ExternalLink = styled('a')`
+  color: #475065;
+`;
+
+export const InfoList = styled('ol')`
+  padding-left: 24px;
+`;
