97ba58d20cd4efc308974ed16366286aeef441d2
Juan La Jara <juanlajara@gmail.com>
2021-03-16 17:35:07 -0600

    Update `BaseButton` with disabled styles
    Maybe we could adapt the logic to have an actual disabled prop
    but I don't know if we'd be overwriting the actual disabled html
    attribute.
    
    Need to look this further.

--- components/BaseButton/styles.js
+++ components/BaseButton/styles.js
@@ -4,24 +4,28 @@ const backgroundColorPriority = {
   primary: '#04102F',
   secondary: '#4EB5A2',
   transparent: 'transparent',
+  disabled: '#8E94A4',
 };
 
 const borderColorPriority = {
   primary: 'none',
   secondary: 'none',
   transparent: '#04102F',
+  disabled: 'none',
 };
 
 const borderWidthPriority = {
   primary: '0px',
   secondary: '0px',
   transparent: '1px',
+  disabled: '0px',
 };
 
 const textColorPriority = {
   primary: 'white',
   secondary: 'white',
   transparent: '#04102F',
+  disabled: 'white',
 };
 
 export const StyledButton = styled('button')`
@@ -30,7 +34,7 @@ export const StyledButton = styled('button')`
   border-color: ${(props) => borderColorPriority[props.type || 'primary']};
   border-width: ${(props) => borderWidthPriority[props.type || 'primary']};
   border-radius: 2rem;
-  cursor: pointer;
+  cursor: ${(props) => (props.type === 'disabled' ? 'auto' : 'pointer')};
   color: ${(props) => textColorPriority[props.type || 'primary']};
   font-family: 'Roboto', sans-serif;
   min-width: 7.375rem;
@@ -38,6 +42,7 @@ export const StyledButton = styled('button')`
   padding: 0 1.5rem;
 
   &:hover {
-    text-decoration: underline;
+    text-decoration: ${(props) =>
+      props.type === 'disabled' ? 'none' : 'underline'};
   }
 `;
