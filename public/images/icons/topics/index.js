c114dc55b80ef23a571d48f455f49c77e2cc9ec4
Juan La Jara <juanlajara@gmail.com>
2021-03-16 18:00:15 -0600

    Update Topic's SVGs
    New topics.

--- public/images/icons/topics/index.js
+++ public/images/icons/topics/index.js
@@ -1,6 +1,8 @@
 export { default as EducationIcon } from './education.svg';
-export { default as HealthIcon } from './health.svg';
-export { default as EconomyIcon } from './economy.svg';
-export { default as GobernabilityIcon } from './gobernability.svg';
-export { default as SecurityIcon } from './security.svg';
 export { default as EnvironmentIcon } from './environment.svg';
+export { default as GovernabilityIcon } from './governability.svg';
+export { default as GrowthIcon } from './growth.svg';
+export { default as HealthIcon } from './health.svg';
+export { default as RightsIcon } from './rights.svg';
+export { default as SecurityIcon } from './security.svg';
+export { default as TaxesIcon } from './taxes.svg';
