a4fe50ef5c557f6d2a81ff4041c528a38331db9f
Paul Ccari <paulclindo@gmail.com>
2021-03-18 23:15:16 -0500

    add userAnswers in context

--- hooks/useTopics/index.js
+++ hooks/useTopics/index.js
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useContext } from 'react';
 import { usePersistedReducer } from 'hooks/usePersistedReducer';
 
 export const TopicsContext = createContext();
@@ -6,6 +6,7 @@ export const TopicsContext = createContext();
 const initialTopicsState = {
   userSelected: [],
   quizItems: {},
+  userAnswers: [],
 };
 
 const reducer = (state, action) => {
@@ -30,6 +31,25 @@ const reducer = (state, action) => {
   if (action.type === 'addQuizItems') {
     return { ...state, quizItems: action.value };
   }
+  if (action.type === 'addUserAnswers') {
+    const answerIndex = state.userAnswers.findIndex(
+      (answer) => answer.questionId === action.value.questionId,
+    );
+
+    if (answerIndex === -1) {
+      return {
+        ...state,
+        userAnswers: [...state.userAnswers, action.value],
+      };
+    } else {
+      const newUserAnswers = [...state.userAnswers];
+      newUserAnswers[answerIndex] = action.value;
+      return {
+        ...state,
+        userAnswers: newUserAnswers,
+      };
+    }
+  }
 };
 
 export const TopicsProvider = ({ children }) => {
@@ -47,9 +67,20 @@ export const TopicsProvider = ({ children }) => {
           dispatch({ type: 'addUserSelectedTopic', value }),
         removeUserSelectedTopic: (value) =>
           dispatch({ type: 'removeUserSelectedTopic', value }),
+        quizItems: state.quizItems,
         addQuizItems: (value) => dispatch({ type: 'addQuizItems', value }),
+        userAnswers: state.userAnswers,
+        addUserAnswers: (value) => dispatch({ type: 'addUserAnswers', value }),
       }}>
       {children}
     </TopicsContext.Provider>
   );
 };
+
+export function useTopics() {
+  const context = useContext(TopicsContext);
+  if (!context) {
+    throw new Error(`useTopics must be used within a TopicsProvider`);
+  }
+  return context;
+}
