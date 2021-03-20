25c426d60c4481bb73966f46963758c9f9655a99
almeald <almenaldana@gmail.com>
2021-03-19 20:00:38 -0500

    fix card styles

--- components/CandidateCard/styles.js
+++ components/CandidateCard/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Image from 'next/image';
 
 const borderPriority = {
-  primary: '#c7cad1',
+  primary: '#e3e5e8',
   secondary: '#4EB5A2',
 };
 
@@ -10,8 +10,8 @@ export const PartyIcon = styled(Image).attrs((props) => ({
   height: 32,
   width: 32,
 }))`
-  border: 1px solid #c7cad1;
   border-radius: 2px;
+  object-fit: contain;
 `;
 
 export const NumberIcon = styled('span')`
@@ -21,6 +21,7 @@ export const NumberIcon = styled('span')`
   font-weight: 700;
   height: 2rem;
   line-height 2rem;
+  margin: 0 0.25rem;
   text-align: center;
   width: 2rem;
 `;
@@ -29,11 +30,11 @@ export const ProfileIcon = PartyIcon;
 
 export const Card = styled('div')`
   align-items: center;
+  background-color: #fff;
   border-radius: 4px;
   border: 1px solid ${(props) => borderPriority[props.type || 'primary']};
   cursor: pointer;
   display: flex;
-  gap: 0.25rem;
   height: 3.75rem;
   justify-content: flex-start;
   padding: 0.875rem;
@@ -47,6 +48,11 @@ export const Fullname = styled('p')`
   font-weight: 700;
   line-height: 1.125rem;
   margin: 0 0 0 0.5rem;
+  text-transform: lowercase;
+
+  &::first-line {
+    text-transform: capitalize;
+  }
 
   ${Card}:hover & {
     text-decoration: underline;
