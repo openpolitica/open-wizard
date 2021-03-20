671f86bf44fbd043fc327a249391dc8fe8ca610a
Paul Ccari <paulclindo@gmail.com>
2021-03-20 13:28:36 -0500

    sort topics

--- public/images/icons/topics/index.js
+++ public/images/icons/topics/index.js
@@ -1,8 +1,8 @@
-export { default as EducationIcon } from './education.svg';
-export { default as EnvironmentIcon } from './environment.svg';
-export { default as GovernabilityIcon } from './governability.svg';
-export { default as GrowthIcon } from './growth.svg';
 export { default as HealthIcon } from './health.svg';
-export { default as RightsIcon } from './rights.svg';
-export { default as SecurityIcon } from './security.svg';
+export { default as EducationIcon } from './education.svg';
+export { default as GrowthIcon } from './growth.svg';
 export { default as TaxesIcon } from './taxes.svg';
+export { default as SecurityIcon } from './security.svg';
+export { default as GovernabilityIcon } from './governability.svg';
+export { default as RightsIcon } from './rights.svg';
+export { default as EnvironmentIcon } from './environment.svg';
