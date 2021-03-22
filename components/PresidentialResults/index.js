6006615944f00ea6419b42023de96a5a0b430db6
Juan La Jara <juanlajara@gmail.com>
2021-03-21 20:01:10 -0600

    Add basic static layout to presidential-results
    Later we'll use the API.

--- components/PresidentialResults/index.js
+++ components/PresidentialResults/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import { useTopics } from 'hooks/useTopics';
+// import { useTopics } from 'hooks/useTopics';
+import * as Styled from './styles';
+import MainLayout from 'components/layouts/MainLayout';
+
+// import Loading from 'components/Loading';
+
+// const LoadingScreen = () => {
+//   return (
+//     <MainLayout>
+//       <Loading />
+//     </MainLayout>
+//   );
+// };
 
 export default function PresidentialResults() {
-  const { userAnswers } = useTopics();
+  // const { userAnswers } = useTopics();
 
   return (
-    <div>
-      TODO: results
-      {userAnswers.map(({ questionId, answerId }) => (
-        <div key={questionId}>
-          {questionId} <span>{answerId || 'omitted answer'}</span>
-        </div>
-      ))}
-    </div>
+    <MainLayout>
+      <Styled.Row>
+        <Styled.GoBackButton to="/" text="Regresa" />
+      </Styled.Row>
+      <Styled.Title>Resultados</Styled.Title>
+      <Styled.Results>
+        <Styled.ThinkLikeYou>Piensa más como tú</Styled.ThinkLikeYou>
+        <Styled.CompatibilityPartyCard
+          partyName="Accion Popular"
+          compatibility={69}
+        />
+        <Styled.OtherResults>Otros resultados</Styled.OtherResults>
+        <Styled.CompatibilityPartyCard />
+        <Styled.CompatibilityPartyCard />
+        <Styled.CompatibilityPartyCard />
+        <Styled.CompatibilityPartyCard />
+      </Styled.Results>
+      <Styled.HorizontalRule />
+      <Styled.Disclaimer>
+        <Styled.Tagline>¿Cómo obtuve estos resultados?</Styled.Tagline>
+        <Styled.CallToAction>
+          Revisa el análisis que hicimos a los planes de gobierno
+        </Styled.CallToAction>
+        <Styled.Text1>
+          El equipo de Open Política analizó los planes de gobierno de los{' '}
+          <strong>
+            10 partidos con mayor intención de voto presidencial al 10 de marzo
+            de 2021.
+          </strong>
+        </Styled.Text1>
+        <Styled.Text2>
+          Si deseas leer el detalle del análisis, puedes descargarlo desde el
+          siguiente enlace:
+        </Styled.Text2>
+        <Styled.DownloadText href="#">
+          <Styled.DownloadIcon />
+          Descargar reporte en PDF
+        </Styled.DownloadText>
+      </Styled.Disclaimer>
+    </MainLayout>
   );
 }
---
+++ components/PresidentialResults/styles.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import {
+  Row as BaseRow,
+  GoBackButton as BaseGoBackButton,
+  Title as BaseTitle,
+  Topics as BaseResults,
+} from 'components/PresidentialTopics/styles';
+import BaseParagraph from 'components/BaseParagraph';
+import BaseCompatibilityPartyCard from 'components/CompatibilityPartyCard';
+import BaseDownloadIcon from 'public/images/icons/download.svg';
+
+export const Row = BaseRow;
+export const GoBackButton = BaseGoBackButton;
+export const Title = BaseTitle;
+export const Tagline = styled(BaseParagraph)`
+  font-weight: bold;
+  width: 100%;
+`;
+
+export const ThinkLikeYou = Tagline;
+export const OtherResults = styled(Tagline)`
+  margin-top: 1.5rem;
+`;
+
+export const Results = styled(BaseResults)`
+  margin-top: 1.5rem;
+`;
+
+export const CompatibilityPartyCard = styled(BaseCompatibilityPartyCard)`
+  margin-top: 0.625rem;
+`;
+
+export const HorizontalRule = styled('hr')`
+  border: 1px solid #e3e5e8;
+  width: 100%;
+`;
+
+export const Disclaimer = Results;
+
+export const CallToAction = styled(Tagline)`
+  font-size: 0.875rem;
+  line-height: 1.375rem;
+`;
+
+export const Text = styled(BaseParagraph)`
+  margin-top: 1rem;
+  width: 100%;
+`;
+
+export const Text1 = Text;
+export const Text2 = styled(Text)`
+  margin-bottom: 1.125rem;
+  margin-top: 1rem;
+`;
+
+export const DownloadIcon = styled(BaseDownloadIcon)`
+  margin-right: 0.625rem;
+  transform: translateY(0.2rem);
+`;
+
+export const DownloadText = styled('a')`
+  color: #4bc540;
+  font-size: 0.875rem;
+  text-decoration: none;
+  width: 100%;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
