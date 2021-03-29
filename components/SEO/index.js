7cdc3aa76aa20358e832eead881fbdfb1e9b2a4d
Paul Ccari <paulclindo@gmail.com>
2021-03-28 19:30:37 -0500

    refactor seo component

--- components/SEO/index.js
+++ components/SEO/index.js
@@ -1,24 +1,20 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
+import { Fragment } from 'react';
 
 export default function SEO(props) {
-  const { ...customMeta } = props;
-  const router = useRouter();
   const meta = {
     title: 'VOTU | Tu guía digital para las elecciones del Perú.',
     description: `Votu.pe es tu guía digital para ayudarte a elegir tu voto. En VOTU.pe puedes encontrar informacion de todos los candidatos congresales y presidenciales, basada en fuentes de datos públicas y planes de gobierno. Mediante distintos filtros, y preguntas, VOTU.pe te ayuda a encontrar a los candidatos que más se alinean con tus preferencias personales. VOTU es una aplicación independiente desarrollada por Open Politica.`,
     image: 'https://votu.pe/images/banner.png',
     type: 'website',
-    ...customMeta,
   };
 
   return (
-    <Head>
+    <Fragment>
       <title>{meta.title}</title>
       <meta name="robots" content="follow, index" />
       <meta content={meta.description} name="description" />
-      <meta property="og:url" content={`https://votu.pe${router.asPath}`} />
-      <link rel="canonical" href={`https://votu.pe${router.asPath}`} />
+      <meta property="og:url" content={`https://votu.pe`} />
+      <link rel="canonical" href={`https://votu.pe`} />
       <meta property="og:type" content={meta.type} />
       <meta property="og:site_name" content="Votu.pe" />
       <meta property="og:description" content={meta.description} />
@@ -32,6 +28,6 @@ export default function SEO(props) {
       {meta.date && (
         <meta property="article:published_time" content={meta.date} />
       )}
-    </Head>
+    </Fragment>
   );
 }
