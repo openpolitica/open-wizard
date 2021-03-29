476a9619cfab919e01fbb399302bdea9d7f506cb
Paul Ccari <paulclindo@gmail.com>
2021-03-28 19:55:45 -0500

    fix imports

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import GlobalStyles from '../global-styles/global';
-import SEO from '../components/SEO';
+import GlobalStyles from 'global-styles/global';
+import SEO from 'components/SEO';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
