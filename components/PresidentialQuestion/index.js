10fdd0256dfe69c021dc152e2a3ee435734fb12e
Paul Ccari <paulclindo@gmail.com>
2021-03-29 21:09:56 -0500

    Allow multiple omits and update UI accordingly

--- components/PresidentialQuestion/index.js
+++ components/PresidentialQuestion/index.js
@@ -2,7 +2,6 @@ import * as Styled from './styles';
 import { Fragment, useEffect, useState } from 'react';
 import BaseRadioButton from 'components/BaseRadioButton';
 import RadioGroup from 'components/BaseRadioButton/RadioGroup';
-import BaseButton from 'components/BaseButton';
 import CheckboxGroup from 'components/BaseCheckbox/CheckboxGroup';
 import BaseCheckbox from 'components/BaseCheckbox';
 import * as icons from 'public/images/icons/topics';
@@ -11,8 +10,6 @@ import { useRouter } from 'next/router';
 import { translationMap } from 'components/TopicCheckbox';
 import { GoBackButton } from 'components/PresidentialTopics/styles';
 import MainLayout from 'components/layouts/MainLayout';
-
-const allowOmittedQuestionsPerTopic = 1;
 
 const nameCapitalized = (string) =>
   string.charAt(0).toUpperCase() + string.slice(1);
@@ -25,7 +22,6 @@ export default function PresidentialQuestion() {
 
   const [currentTopic, setCurrentTopic] = useState(0);
   const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
-  const [questionsOmitted, setQuestionsOmitted] = useState(0);
 
   const router = useRouter();
   const { addUserAnswers, quizItems } = useTopics();
@@ -54,9 +50,6 @@ export default function PresidentialQuestion() {
       ? Array.from(selectedOption.answers)
       : null;
 
-    if (isOmitted) {
-      setQuestionsOmitted((prevIdx) => prevIdx + 1);
-    }
     if (isLowerThanLastQuestionIdx) {
       addUserAnswers({
         questionId,
@@ -66,7 +59,6 @@ export default function PresidentialQuestion() {
       setCurrentQuestionIdx((prevIdx) => prevIdx + 1);
     }
     if (isLastCurrentQuestionIdx && !isLastCurrentTopicIdx) {
-      setQuestionsOmitted(0);
       setCurrentTopic((prevIdx) => prevIdx + 1);
       setCurrentQuestionIdx(0);
       addUserAnswers({
@@ -170,18 +162,18 @@ export default function PresidentialQuestion() {
             )}
           </Styled.QuestionList>
           <Styled.QuestionButtons>
-            <BaseButton
+            <Styled.OmitButton
+              disabled={!shouldContinueButtonBeDisabled}
+              onClick={() => handleNextButton({ isOmitted: true })}
+              type="transparent">
+              No responder
+            </Styled.OmitButton>
+            <Styled.NextButton
               type={shouldContinueButtonBeDisabled ? 'disabled' : 'primary'}
               disabled={shouldContinueButtonBeDisabled}
               onClick={handleNextButton}>
               Continuar
-            </BaseButton>
-            <Styled.OmitButton
-              disabled={questionsOmitted === allowOmittedQuestionsPerTopic}
-              onClick={() => handleNextButton({ isOmitted: true })}
-              type="transparent">
-              Omitir
-            </Styled.OmitButton>
+            </Styled.NextButton>
           </Styled.QuestionButtons>
         </Fragment>
       ) : (
