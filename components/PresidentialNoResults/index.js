8486bff4dcb3cfb71ae87eb52674faad12cba736
Juan La Jara <juanlajara@gmail.com>
2021-04-03 23:36:58 -0600

    Update `minimumRequiredNumberOfAnswers` to 1
    We have topics with just 3 questions, so we want to lower
    the barrier to the minimum.

--- components/PresidentialNoResults/index.js
+++ components/PresidentialNoResults/index.js
@@ -3,8 +3,11 @@ import Router from 'next/router';
 import { Fragment } from 'react';
 import GoBackButton from 'components/GoBackButton';
 import * as Styled from './styles';
+import simplePluralize from 'components/Steps/PartyResults/simplePluralize';
 
-export default function PresidentialNoResults() {
+export default function PresidentialNoResults({
+  minimumRequiredNumberOfAnswers,
+}) {
   return (
     <Fragment>
       <GoBackButton to="/presidential-steps/2" text="Regresa" />
@@ -12,7 +15,9 @@ export default function PresidentialNoResults() {
       <Styled.Chip type="bad">
         <strong>No te pases, pues.</strong>
         <Styled.Paragraph>
-          Responde al menos 3 preguntas para obtener un resultado.
+          Responde al menos{' '}
+          {simplePluralize(minimumRequiredNumberOfAnswers, 'pregunta')} para
+          obtener un resultado.
         </Styled.Paragraph>
       </Styled.Chip>
       <Image
